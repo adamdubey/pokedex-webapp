e4d406a093f7545b1efdbcc4053941341e5973d7
Adam Dubey <adubey0311@gmail.com>
2021-08-12 16:38:56 -0400

    [AD][08M1221] Gotta catch em all

---
+++ src/Pokegame.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Pokedex from './Pokedex';
+
+class Pokegame extends Component {
+    static defaultProps = {
+        pokemon : [
+            { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
+            { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
+            { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
+            { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
+            { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
+            { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
+            { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
+            { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
+        ]
+    };
+render() {
+    let playerOne = [];
+    let playerTwo = [ ...this.props.pokemon ];
+    while (playerOne.length < playerTwo.length) {
+        let randIdx = Math.floor(Math.random() * playerTwo.length);
+        let randPokemon = playerTwo.splice(randIdx, 1)[0];
+        playerOne.push(randPokemon);
+    }
+    let expOne = playerOne.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+    let expTwo = playerTwo.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+    return (
+        <div>
+            <Pokedex pokemon={playerOne} exp={expOne} isWinner={expOne > expTwo} />
+            <Pokedex pokemon={playerTwo} exp={expTwo} isWinner={expTwo > expOne} />
+        </div>
+    );
+}
+}
+
+export default Pokegame;
